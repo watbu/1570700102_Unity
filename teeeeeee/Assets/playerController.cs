3fb89f0bbb5c47294056f09212115bda603fd75d
TEERAWAT EAKSIRIWONG <teerawat.eaks@bulive.net>
2016-01-27 11:33:29 +0700

    MOVING THE PLAYER

---
+++ teeeeeee/Assets/playerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+public class playerController : MonoBehaviour {
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+	
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		var movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		var rigidbody = GetComponent<Rigidbody> ();
+		
+		rigidbody.velocity = movement * speed;
+		
+		rigidbody.position = new Vector3 (
+			Mathf.Clamp (rigidbody.position.x, boundary.xMin, boundary.xMax),
+			0.0f,
+			Mathf.Clamp (rigidbody.position.z, boundary.zMin, boundary.zMax)
+			);
+		
+		rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
