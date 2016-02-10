da456cd0b5889079a147f76102622da00e629d62
TEERAWAT EAKSIRIWONG <teerawat.eaks@bulive.net>
2016-02-10 11:40:46 +0700

    Creating hazards

---
+++ teeeeeee/Assets/scripts/destroybycontract.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroybycontract : MonoBehaviour {
+
+	void Start(){
+	}
+	void Update(){
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.tag == "boundary") {
+			return;
+		} else
+			Destroy (other.gameObject);
+		Destroy (gameObject);﻿
+	}
+}
---
+++ teeeeeee/Assets/scripts/randomrotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class randomrotator : MonoBehaviour {
+	public float tumbel;
+	void Start(){
+		Rigidbody rb;
+		rb = GetComponent<Rigidbody> ();
+		rb.angularVelocity = Random.insideUnitSphere * tumbel;
+	}
+
+}
