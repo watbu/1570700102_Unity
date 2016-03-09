1879b19e754954b7c0a2e589846ba9c7321eb035
TEERAWAT EAKSIRIWONG <teerawat.eaks@bulive.net>
2016-03-09 11:06:05 +0700

    Counting points and displaying the score

--- teeeeeee/Assets/scripts/GameController.cs
+++ teeeeeee/Assets/scripts/GameController.cs
@@ -10,29 +10,12 @@ public class GameController : MonoBehaviour {
 	public float waveWait;
 
 	public GUIText scoreText;
-	public GUIText restartText;
-	public GUIText gameOverText;
-
-	private bool gameOver;
-	private bool restart;
 	private int score;
 	void Start()
 	{
-		gameOver = false;
-		restart = false;
-		restartText.text = "";
-		gameOverText.text = "";
 		score = 0;
 		UpdateScore ();
 		StartCoroutine(SpawnWaves());
-	}
-	void Update()
-	{
-		if (restart) {
-			if (Input.GetKeyDown (KeyCode.R)) {
-				Application.LoadLevel (Application.loadedLevel);
-			}
-		}
 	}
 	IEnumerator SpawnWaves ()
 	{
@@ -48,13 +31,6 @@ public class GameController : MonoBehaviour {
 		yield return new WaitForSeconds (spawnWait);
 		}
 			yield return new WaitForSeconds (waveWait);
-
-			if (gameOver)
-			{
-				restartText.text = "Press 'R' for Restart !!";
-				restart = true;
-				break;
-			}
 		}
 	}
 	public void AddScore (int newScoreValue)
@@ -65,11 +41,6 @@ public class GameController : MonoBehaviour {
 	void UpdateScore()
 	{
 		scoreText.text = "Score" + score;
-	}
-	public void GameOver()
-	{
-		gameOverText.text = "Game Over";
-		gameOver = true;
 	}
 }
 
--- teeeeeee/Assets/scripts/destroybycontract.cs
+++ teeeeeee/Assets/scripts/destroybycontract.cs
@@ -25,7 +25,6 @@ public class destroybycontract : MonoBehaviour {
 		Instantiate(explosion, transform.position,transform.rotation);
 		if (other.tag == "Player") {
 			Instantiate(playerExplosion, other.transform.position,other.transform.rotation);
-			gameController.GameOver();
 		}
 		gameController.AddScore(scoreValue);
 		Destroy (other.gameObject);
