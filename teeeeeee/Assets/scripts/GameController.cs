1d9b5fff612fa6798b5cfde112955b7cee5e8375
TEERAWAT EAKSIRIWONG <teerawat.eaks@bulive.net>
2016-03-09 11:01:39 +0700

    Ending the game

--- teeeeeee/Assets/scripts/GameController.cs
+++ teeeeeee/Assets/scripts/GameController.cs
@@ -8,9 +8,31 @@ public class GameController : MonoBehaviour {
 	public float spawnWait;
 	public float startWait;
 	public float waveWait;
+
+	public GUIText scoreText;
+	public GUIText restartText;
+	public GUIText gameOverText;
+
+	private bool gameOver;
+	private bool restart;
+	private int score;
 	void Start()
 	{
+		gameOver = false;
+		restart = false;
+		restartText.text = "";
+		gameOverText.text = "";
+		score = 0;
+		UpdateScore ();
 		StartCoroutine(SpawnWaves());
+	}
+	void Update()
+	{
+		if (restart) {
+			if (Input.GetKeyDown (KeyCode.R)) {
+				Application.LoadLevel (Application.loadedLevel);
+			}
+		}
 	}
 	IEnumerator SpawnWaves ()
 	{
@@ -26,7 +48,28 @@ public class GameController : MonoBehaviour {
 		yield return new WaitForSeconds (spawnWait);
 		}
 			yield return new WaitForSeconds (waveWait);
+
+			if (gameOver)
+			{
+				restartText.text = "Press 'R' for Restart !!";
+				restart = true;
+				break;
+			}
 		}
+	}
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore ();
+	}
+	void UpdateScore()
+	{
+		scoreText.text = "Score" + score;
+	}
+	public void GameOver()
+	{
+		gameOverText.text = "Game Over";
+		gameOver = true;
 	}
 }
 
--- teeeeeee/Assets/scripts/destroybycontract.cs
+++ teeeeeee/Assets/scripts/destroybycontract.cs
@@ -5,6 +5,18 @@ public class destroybycontract : MonoBehaviour {
 	
 	public GameObject explosion;
 	public GameObject playerExplosion;
+	public int scoreValue;
+	private GameController gameController;
+	void Start()
+	{
+		GameObject gameControllerobject = GameObject.FindWithTag ("GameController");
+		if (gameControllerobject != null) {
+			gameController = gameControllerobject.GetComponent <GameController> ();
+		}
+		if (gameController == null) {
+			Debug.Log ("Cannot find 'GameController' Script");
+		}
+	}
 	void OnTriggerEnter (Collider other)
 	{
 		if (other.tag == "boundary") {
@@ -13,8 +25,11 @@ public class destroybycontract : MonoBehaviour {
 		Instantiate(explosion, transform.position,transform.rotation);
 		if (other.tag == "Player") {
 			Instantiate(playerExplosion, other.transform.position,other.transform.rotation);
+			gameController.GameOver();
 		}
-			Destroy (other.gameObject);
+		gameController.AddScore(scoreValue);
+		Destroy (other.gameObject);
 		Destroy (gameObject);
+
 	}
 }
