42cf698854555e61892fb024097cb4e1853efb2a
watbu <teerawat.eaks@bumail.net>
2016-05-05 00:25:38 +0700

    Final

---
+++ Cannon/Assets/ShootController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class ShootController : MonoBehaviour {
+	private Slider mSlider; 
+	private ammo_addforce conammo; 
+	public GameObject ammo; 
+	void Awake()
+	{
+		mSlider = this.GetComponent<Slider> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		GameObject isconammo = GameObject.FindGameObjectWithTag ("Ammo");
+		if (isconammo != null) {
+			conammo = isconammo.GetComponent<ammo_addforce>();
+		}
+		if (isconammo == null) {
+			Debug.Log("Error");
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		OnSlider ();
+		
+	}
+
+	public void OnSlider()
+	{
+		  conammo.setspeed = mSlider.value;
+	}
+
+}
---
+++ Cannon/Assets/Wall_Tranform.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall_Tranform : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody> ().velocity = new Vector3 (0f,10f,0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+	}
+	void OnCollisionEnter(Collision other)
+
+	{
+		if (other.gameObject.tag == "Wall2") {
+			GetComponent<Rigidbody> ().velocity = new Vector3 (0f,-10f,0f);
+		}
+		if (other.gameObject.tag == "Wall3") {
+			GetComponent<Rigidbody> ().velocity = new Vector3 (0f,10f,0f);
+		}
+	}
+}
